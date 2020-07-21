674cb6a097857354b70da782acabc70454111f55
Zhang Kangkang <zhang_kangkang@foxmail.com>
2020-07-21 23:53:48 +0800

    方法和参数

---
+++ 方法和参数/FucData/Class1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FucData2
+{
+    class Class1
+    {
+        public static void GetClass1() {
+            Console.WriteLine("class1");
+        }
+    }
+}
