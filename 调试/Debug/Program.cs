fcb63c80595031342ef2227427d9942d9b3b8bb3
Zhang Kangkang <zhang_kangkang@foxmail.com>
2020-08-10 23:34:11 +0800

    调试，复杂数据类型

---
+++ 调试/Debug/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Debug
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float a = 0;
+            Console.WriteLine(a++);
+            Console.WriteLine(a++);
+            Console.WriteLine(a++);
+            Console.WriteLine(a++);
+            Console.WriteLine(a++);
+            Console.WriteLine(a++);
+            Console.WriteLine(a++);
+            Console.WriteLine(a++);
+            Console.WriteLine(a++);
+            Console.WriteLine(a++);
+
+            string str = "dog";
+            try
+            {
+                Console.WriteLine(Convert.ToInt32(str));
+            }
+            catch
+            {
+                Console.WriteLine("error");
+            }
+            
+        }
+    }
+}
