cece301a02ab18dd4bdfa8321ca05f281fa81b1a
zhang-kangkang <zhang_kangkang@foxmail.com>
2020-08-11 15:04:13 +0800

    析构函数，洛谷答题

---
+++ 洛谷答题/LuoGu/Class1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+public class Class1
+{
+	public Class1()
+	{
+        // P1035 级数求和
+        int K = int.Parse(Console.ReadLine());
+        double sum = 0;
+        double n;
+        Console.WriteLine("000");
+        for (double i = 0;i<10 ; i++)
+        {
+            // sum += (1 / i);
+            Console.WriteLine("000");
+            if(sum>K){
+                n = i;
+                Console.WriteLine(n);
+                break;
+            }
+        }
+        
+    }
+}
