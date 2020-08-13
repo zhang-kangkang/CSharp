eb4b23b3296598efc138e74b0d573d9cffc163eb
zhang-kangkang <zhang_kangkang@foxmail.com>
2020-08-13 16:58:14 +0800

    飞行棋-地图字符输出

--- 飞行棋/FlyChess/Program.cs
+++ 飞行棋/FlyChess/Program.cs
@@ -19,7 +19,7 @@ namespace FlyChess
         static void Main(string[] args)
         {
             ShowHead();
-            Console.WriteLine(maps[1]);
+            Console.WriteLine("");
             InitMap();
             ShowMap();
 
@@ -36,7 +36,7 @@ namespace FlyChess
             Console.ForegroundColor = ConsoleColor.Magenta;
             Console.WriteLine("*************************");
             Console.ForegroundColor = ConsoleColor.Yellow;
-            Console.WriteLine("**********飞行棋*********");
+            Console.WriteLine("*************************");
             Console.ForegroundColor = ConsoleColor.DarkYellow;
             Console.WriteLine("*************************");
             Console.ForegroundColor = ConsoleColor.Cyan;
@@ -57,9 +57,9 @@ namespace FlyChess
         {
             //0方块，1幸运轮盘，2地雷，3暂停,4传送门
             int[] luckyTurn = { 3, 26, 77 };
-            int[] landMine = { 13, 16, 87 };
+            int[] landMine = { 13, 26, 87 };
             int[] pause = { 44, 54, 94 };
-            int[] portDoor = { 6, 36, 66 };
+            int[] portDoor = { 9, 36, 66 };
             SetInitMapValue(luckyTurn,1);
             SetInitMapValue(landMine, 2);
             SetInitMapValue(pause, 3);
@@ -76,41 +76,95 @@ namespace FlyChess
 
         static void ShowMap()
         {
-            for (int i = 0; i < maps.Length; i++)
+            Console.ForegroundColor = ConsoleColor.DarkGray;
+            Console.WriteLine("提示：");
+            Console.WriteLine("     □方块，★幸运轮盘，◎地雷，▲暂停,卍传送门");
+            Console.WriteLine("     A玩家1位置，B玩家2位置，<>两个玩家都在该位置");
+            Console.WriteLine("");
+            for (int i = 0; i < 30; i++)
             {
-                if(i == player1 && i == player2)
+                Console.Write(getMapELement(i));
+                if (i == 29)
                 {
-                    Console.Write("<>");
-                    continue;
-                }else if(i == player1)
-                {
-                    Console.Write("A");
-                    continue;
+                    Console.WriteLine("");
                 }
-                else if (i == player2)
+            }
+            for (int i = 30; i < 35; i++)
+            {
+                Console.WriteLine("                                                          {0}", getMapELement(i));
+            }
+            for (int i = 64; i > 34; i--)
+            {
+                Console.Write(getMapELement(i));
+                if (i == 35)
                 {
-                    Console.Write("B");
-                    continue;
+                    Console.WriteLine("");
                 }
+            }
+            for (int i = 65; i < 70; i++)
+            {
+                Console.WriteLine("{0}                                                        ", getMapELement(i));
+            }
+            for (int i = 70; i < 100; i++)
+            {
+                Console.Write(getMapELement(i));
+                if (i == 99)
+                {
+                    Console.WriteLine("");
+                }
+            }
+        }
+
+        static string getMapELement(int i){
+            // 有玩家位于该位置时
+            if (i == player1 || i == player2)
+            {
+                if (i == player1 && i == player2)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    return "<>";
+                }
+                else if (i == player1)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    return "A";
+                }
+                else
+                {
+                    Console.ForegroundColor = ConsoleColor.Blue;
+                    return "B";
+                }
+            }
+            else
+            {
                 switch (maps[i])
                 {
                     case 0:
-                        Console.Write("0");
-                        continue;
+                        Console.ForegroundColor = ConsoleColor.DarkGray;
+                        return "□";
                     case 1:
-                        Console.Write("1");
-                        continue;
+                        Console.ForegroundColor = ConsoleColor.DarkYellow;
+                        return "★";
                     case 2:
-                        Console.Write("2");
-                        continue;
+                        Console.ForegroundColor = ConsoleColor.Yellow;
+                        return "◎";
                     case 3:
-                        Console.Write("3");
-                        continue;
+                        Console.ForegroundColor = ConsoleColor.DarkMagenta;
+                        return "▲";
                     case 4:
-                        Console.Write("4");
-                        continue;
+                        Console.ForegroundColor = ConsoleColor.Green;
+                        return "卍";
+                    default:
+                        Console.ForegroundColor = ConsoleColor.DarkCyan;
+                        return "□";
                 }
             }
+
+        }
+
+        static void GameRound()
+        {
+
         }
 
         static void showWinner()
